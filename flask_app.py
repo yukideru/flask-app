8c3c8f992ffcfe9f14bb7072c0bb73bffc1e1a40
yukideru <yuki70506@gmail.com>
2025-03-28 15:49:26 +0800

    Add yuki

--- flask_app.py
+++ flask_app.py
@@ -9,6 +9,9 @@ def home():
 def test():
     return "This is test"
 
+@app.route("/yuki") #代表我們要處理的網路路徑
+def yuki():
+    return "yuki's website"
 
 if __name__=="__main__": #如果以主程式執行
     app.run()  # 立刻啟動伺服器
